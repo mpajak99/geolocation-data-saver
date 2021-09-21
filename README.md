1f4252d9121ca23a0946e2cdfcaed144e82240eb
mpajak <mpajak_99@wp.pl>
2021-09-21 10:57:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# geolocation-data-saver
---
+++ src/main/java/com/example/geolocationdatasaver/GeolocationDataSaverApplication.java
@@ -0,0 +1,13 @@
+package com.example.geolocationdatasaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeolocationDataSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeolocationDataSaverApplication.class, args);
+	}
+
+}
